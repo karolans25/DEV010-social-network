72d32b77215cc9d96efb724a00741b0aedf91b89
Carolina Pulido Gómez <priyacarito@gmail.com>
2023-08-13 02:58:50 -0500

    style for signin view

--- src/components/signin.js
+++ src/components/signin.js
@@ -2,47 +2,49 @@
 
 function signin(navigateTo) {
   const section = document.createElement('section');
+  const figure = document.createElement('figure');
+  const img = document.createElement('img');
   const title = document.createElement('h2');
-  const buttonReturn = document.createElement('button');
   const form = document.createElement('form');
   const labelEmail = document.createElement('label');
   const inputEmail = document.createElement('input');
   const labelPass = document.createElement('label');
   const inputPass = document.createElement('input');
   const buttonSignIn = document.createElement('button');
+  const op1 = document.createElement('section');
   const buttonSignInGoogle = document.createElement('button');
-  const aRecoverPassword = document.createElement('a');
-  const aSignUp = document.createElement('a');
+  const op2 = document.createElement('section');
+  const buttonSignInGithub = document.createElement('button');
+  const recoverPass = document.createElement('a');
+  const signUp = document.createElement('a');
 
+  // section
+  section.className = 'signin';
+
+  // image
+  img.src = './assets/icons/gorra.png';
+  img.alt = 'user img';
+  figure.append(img);
+
+  // title
   title.textContent = 'Sign In';
-  form.className = 'signin';
 
+  // form
   labelEmail.innerHTML = 'Email: ';
-  labelPass.innerHTML = 'Password: ';
-  buttonReturn.textContent = 'Return to home';
-  buttonSignIn.textContent = 'Sign In';
-  buttonSignInGoogle.textContent = 'Sign In with Google';
-  aRecoverPassword.innerHTML = 'Recover password';
-  aSignUp.innerHTML = 'Sign Up';
-
-  aRecoverPassword.href = '/password';
-  aSignUp.href = '/signup';
-
-  inputEmail.name = 'email';
-  inputPass.name = 'pass';
-
-  inputPass.placeholder = 'Write pass';
-  inputEmail.placeholder = 'Write email';
-
   labelEmail.htmlFor = inputEmail.name;
-  labelPass.htmlFor = inputPass.name;
-
+  inputEmail.name = 'email';
+  inputEmail.placeholder = 'Write email';
   inputEmail.type = 'email';
-  inputPass.type = 'password';
-  buttonSignIn.type = 'submit';
-
   inputEmail.required = true;
+  labelPass.innerHTML = 'Password: ';
+  labelPass.htmlFor = inputPass.name;
+  inputPass.name = 'pass';
+  inputPass.placeholder = 'Write password';
+  inputPass.type = 'password';
   inputPass.required = true;
+  buttonSignIn.textContent = 'Sign In';
+  buttonSignIn.type = 'submit';
+  form.append(labelEmail, inputEmail, labelPass, inputPass, buttonSignIn, recoverPass);
 
   form.addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -52,20 +54,34 @@ function signin(navigateTo) {
     // } catch (err) {
     //   console.error(err);
     // }
-    // navigateTo('/home');
+    navigateTo('/');
   });
 
+  // sign in with Google
+  op1.className = 'google';
+  buttonSignInGoogle.textContent = 'Sign In with Google';
   buttonSignInGoogle.addEventListener('click', () => {
     // signInAuthGoogle();
     // navigateTo('/home');
   });
+  op1.append(buttonSignInGoogle);
 
-  buttonReturn.addEventListener('click', () => {
-    navigateTo('/');
+  // sign in with Github
+  op2.className = 'github';
+  buttonSignInGithub.textContent = 'Sign In with Github';
+  buttonSignInGithub.addEventListener('click', () => {
+    // signInAuthGoogle();
+    // navigateTo('/home');
   });
+  op2.append(buttonSignInGithub);
 
-  form.append(labelEmail, inputEmail, labelPass, inputPass, buttonSignIn);
-  section.append(title, form, buttonSignInGoogle, buttonReturn, aRecoverPassword, aSignUp);
+  // links
+  recoverPass.innerHTML = 'Recover password';
+  recoverPass.href = '/password';
+  signUp.innerHTML = 'Sign Up';
+  signUp.href = '/signup';
+
+  section.append(figure, title, form, op1, op2, signUp);
 
   return section;
 }
