6477212f1a734b8857bd25a9b0ae7a9b6c78f33d
Carolina Pulido Gómez <priyacarito@gmail.com>
2023-08-16 17:49:50 -0500

    changes before restructure the code

--- src/components/feed.js
+++ src/components/feed.js
@@ -2,10 +2,13 @@ function feed(navigateTo) {
   const section = document.createElement('section');
   const title = document.createElement('h2');
   const p = document.createElement('p');
+  const ul = document.createElement('ul');
 
   section.className = 'feed';
   title.innerHTML = 'Feed';
   p.innerHTML = 'Welcome to the app';
+
+  for 
 
   section.append(title, p);
 
