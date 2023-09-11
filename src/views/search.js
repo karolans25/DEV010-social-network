e2b853d6d32c0facf66e72754b66e2a6ed67a1f1
Carolina Pulido Gómez <priyacarito@gmail.com>
2023-09-10 20:25:38 -0500

    search posts of users by name o email

--- src/views/search.js
+++ src/views/search.js
@@ -8,6 +8,7 @@ import { navbar } from './navbar';
 import { popup } from './popup';
 import { TITLE } from '../consts/const';
 import { searchHandler } from '../handlers/searchHandler';
+import { feedHandler } from '../handlers/feedHandler';
 
 export const search = async (navigateTo) => {
   const section = document.createElement('section');
@@ -15,7 +16,7 @@ export const search = async (navigateTo) => {
   const head = await header(navigateTo);
   const nav = navbar(navigateTo);
   const title = document.createElement('h2');
-  const sectionSearch = document.createElement('section');
+  // const sectionSearch = document.createElement('section');
   const subtitle = document.createElement('h2');
   const searchText = document.createElement('input');
   const sectionGetAllPosts = document.createElement('section');
@@ -26,46 +27,117 @@ export const search = async (navigateTo) => {
   subtitle.style.marginTop = '20px';
   title.innerHTML = 'Results';
   title.style.marginTop = '10px';
-  sectionSearch.className = 'search-user';
+  // sectionSearch.className = 'search-user';
   subtitle.textContent = 'Search user:';
   subtitle.style.fontSize = '25px';
   searchText.type = 'text';
   searchText.className = 'post-text';
   searchText.placeholder = 'Search the user';
 
-  searchText.addEventListener('keyup', async (e) => {
-    try {
-      e.preventDefault();
-      console.log(searchText.value);
-      const users = await searchHandler.getAllUsers();
-      console.log(users);
-    } catch (err) { popup(err.message); }
-  });
-
-  const q = query(collection(db, 'post'), orderBy('createdAt', 'desc'));
-  onSnapshot(q, (snapshot) => {
-    sectionGetAllPosts.innerHTML = '';
-    const posts = [];
-    snapshot.docs.forEach((documentPost) => {
-      posts.push({ ...documentPost.data(), id: documentPost.id });
+  let ids = [];
+  let users = [];
+  let posts = [];
+  searchHandler.getAllUsers()
+    .then((theUsers) => {
+      theUsers.forEach((user) => {
+        ids.push(user.id);
+        users.push(user);
+      });
+    })
+    .then(async () => {
+      posts = await feedHandler.getAllPost();
+      if (posts.length === 0) {
+        // popup('The user doesn\'t exist! 😓');
+        const text = document.createElement('h2');
+        text.innerHTML = 'The user doesn\'t exist! 😓';
+        sectionGetAllPosts.appendChild(text);
+      }
+      sectionGetAllPosts.innerHTML = '';
+      posts.forEach((item) => {
+        if (ids.includes(item.idUser)) {
+          const formatForEachPost = formatPost(item);
+          formatForEachPost.classList.add('show-post', item.id);
+          console.log(formatForEachPost);
+          sectionGetAllPosts.append(formatForEachPost);
+        }
+      });
+    })
+    .then(() => {
+      searchText.addEventListener('keyup', async (e) => {
+        e.preventDefault();
+        ids = [];
+        users = await searchHandler.getAllUsers();
+        users.forEach((itemUser) => {
+          const searchName = itemUser.name.toLowerCase();
+          const searchEmail = itemUser.email.toLowerCase();
+          const searchArea = searchText.value.toLowerCase();
+          if (searchName.includes(searchArea) || searchEmail.includes(searchArea)) {
+            ids.push(itemUser.id);
+          }
+        });
+        sectionGetAllPosts.innerHTML = '';
+        posts.forEach((item) => {
+          if (ids.includes(item.idUser)) {
+            const formatForEachPost = formatPost(item);
+            formatForEachPost.classList.add('show-post', item.id);
+            console.log(formatForEachPost);
+            sectionGetAllPosts.append(formatForEachPost);
+          }
+        });
+        console.log('ids: ', ids);
+      });
     });
-    if (posts.length === 0) {
-      popup('You don\'t have any post yet');
-      const text = document.createElement('h2');
-      text.innerHTML = '😓 There\'s no post yet!<br>This is your chance to start 😎🥳';
-      sectionGetAllPosts.appendChild(text);
-    }
-    sectionGetAllPosts.innerHTML = '';
-    posts.forEach(async (item) => {
-      const formatForEachPost = await formatPost(item);
-      formatForEachPost.classList.add('show-post', item.id);
-      sectionGetAllPosts.append(formatForEachPost);
-    });
-  });
+  // const users = await searchHandler.getAllUsers();
+  // users.forEach((itemUser) => {
+  //   ids.push(itemUser.id);
+  // });
+  // console.log(ids);
 
-  sectionSearch.appendChild(subtitle);
-  sectionSearch.appendChild(searchText);
-  subSection.append(sectionSearch, title, sectionGetAllPosts);
+  // const posts = await feedHandler.getAllPost();
+  // if (posts.length === 0) {
+  //   // popup('The user doesn\'t exist! 😓');
+  //   const text = document.createElement('h2');
+  //   text.innerHTML = 'The user doesn\'t exist! 😓';
+  //   sectionGetAllPosts.appendChild(text);
+  // }
+  // sectionGetAllPosts.innerHTML = '';
+  // posts.forEach((item) => {
+  //   if (ids.includes(item.idUser)) {
+  //     const formatForEachPost = formatPost(item);
+  //     formatForEachPost.classList.add('show-post', item.id);
+  //     console.log(formatForEachPost);
+  //     sectionGetAllPosts.append(formatForEachPost);
+  //   }
+  // });
+
+  // searchText.addEventListener('keyup', async (e) => {
+  //   e.preventDefault();
+  //   ids = [];
+  //   users = await searchHandler.getAllUsers();
+  //   users.forEach((itemUser) => {
+  //     const searchName = itemUser.name.toLowerCase();
+  //     const searchEmail = itemUser.email.toLowerCase();
+  //     const searchArea = searchText.value.toLowerCase();
+  //     if (searchName.includes(searchArea) || searchEmail.includes(searchArea)) {
+  //       ids.push(itemUser.id);
+  //     }
+  //   });
+  //   sectionGetAllPosts.innerHTML = '';
+  //   posts.forEach((item) => {
+  //     if (ids.includes(item.idUser)) {
+  //       const formatForEachPost = formatPost(item);
+  //       formatForEachPost.classList.add('show-post', item.id);
+  //       console.log(formatForEachPost);
+  //       sectionGetAllPosts.append(formatForEachPost);
+  //     }
+  //   });
+  //     console.log('ids: ', ids);
+  // });
+
+  // sectionSearch.appendChild(subtitle);
+  // sectionSearch.appendChild(searchText);
+  // subSection.append(sectionSearch, title, sectionGetAllPosts);
+  subSection.append(subtitle, searchText, title, sectionGetAllPosts);
   section.append(head, subSection, nav);
 
   return section;
